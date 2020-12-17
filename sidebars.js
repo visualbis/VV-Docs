3dad78cb2604cb4686f2c6684631935622a6f4fd
divyabhushan <divya_bhushan@hotmail.com>
2020-12-17 10:00:15 +0100

    Improved the upgrade page

--- sidebars.js
+++ sidebars.js
@@ -60,8 +60,8 @@ module.exports = {
             "installation-guide/advanced/services-info",
             "installation-guide/advanced/server-restart-checklist",
         ],
-        "Upgrade Trial Version": [
-            "installation-guide/upgrade-trial/upgrade-trial"
+        "Upgrade of BI Hub": [
+            "installation-guide/upgrade/upgrade-bihub"
         ],
 
         "Moving BI Hub instance": ["installation-guide/move-instance/move-instance"],
